962ab3b7bd84e7b9c810e805857d78b1a64ce225
Gabriel xavier <92322369+Gabrieldaybion@users.noreply.github.com>
2023-07-13 14:35:05 -0300

    Foi criado tela projetos

---
+++ Meus projetos/daybion - Copia/javspt/pegar.js
@@ -0,0 +1,32 @@
+/* Projetos de cursos */
+function music(){
+    console.log('Imagem music carregada.')
+    const img = document.getElementById("fotinx");
+    const al = document.getElementById('alink');
+    al.setAttribute('href', 'https://gabrieldaybion.github.io/html-css/desafio/produto-pedido/index.html');
+    img.setAttribute('src', './imagens/app-musica.PNG');
+}
+
+function perf(){
+    console.log('Imagem perfume carregada');
+    const img = document.getElementById('fotinx');
+    const al = document.getElementById('alink');
+    al.setAttribute('href', 'https://gabrieldaybion.github.io/html-css/desafio/card-product/index.html');
+    img.setAttribute('src', './imagens/Site-perfume.PNG');
+}
+function login(){
+    console.log('Imagem login carregada');
+    const img = document.getElementById('fotinx');
+    const al = document.getElementById('alink');
+    al.setAttribute('href', 'https://gabrieldaybion.github.io/projeto-login/');
+    img.setAttribute('src', './imagens/login.PNG');
+}
+
+function cordel(){
+    console.log('Imagem condel carregada');
+    const img = document.getElementById('fotinx');
+    const al = document.getElementById('alink');
+    al.setAttribute('href', 'https://gabrieldaybion.github.io/projeto-login/');
+    img.setAttribute('src', './imagens/cordel.PNG');
+}
+/* Projetos pessoais.*/
