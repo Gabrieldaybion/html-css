c8bf7f200475cf63afdb0671086ea59b7d628866
Gabriel xavier <92322369+Gabrieldaybion@users.noreply.github.com>
2023-09-02 21:12:34 -0300

    Foi criado mais uma fture
    Foi criado a verificação de se é um email ou não.

--- desafio/fm02/newsletter-sign-up-with-success-message-main/assets/js/script2.js
+++ desafio/fm02/newsletter-sign-up-with-success-message-main/assets/js/script2.js
@@ -5,14 +5,19 @@ const btn = document.getElementById('btnemail');
 function enviarMsg(){
     var ms = document.getElementById('txtemail');
     msg = String(ms.value);
-    console.log(msg)
-    
+    console.log(msg);
+    const regexEmail = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
     if (msg == ""){
         window.alert('Desculpe insira um email para continuar')
         
     }else{
-        window.location.href = "confirm.html"
-        window.location.href = `confirm.html?mensagem=${msg}`;
+        if(regexEmail.test(msg)){
+            window.location.href = "confirm.html"
+            window.location.href = `confirm.html?mensagem=${msg}`;
+        }else{
+            window.alert('Desculpe mas email invalido.')
+        }
+        
     }
    
     
