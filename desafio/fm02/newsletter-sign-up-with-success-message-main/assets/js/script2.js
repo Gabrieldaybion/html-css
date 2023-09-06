5ce79f29468540914a9fb004668b629f5c202d40
Gabriel xavier <92322369+Gabrieldaybion@users.noreply.github.com>
2023-09-06 12:58:26 -0300

    Versão completa
    Versão para celular e pc com validação no input de email.

--- desafio/fm02/newsletter-sign-up-with-success-message-main/assets/js/script2.js
+++ desafio/fm02/newsletter-sign-up-with-success-message-main/assets/js/script2.js
@@ -4,18 +4,22 @@ console.log(`Variavel fora da função ${msg}`);
 const btn = document.getElementById('btnemail');
 function enviarMsg(){
     var ms = document.getElementById('txtemail');
+    var txterr = document.getElementById('emaillabel');
     msg = String(ms.value);
     console.log(msg);
     const regexEmail = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
     if (msg == ""){
-        window.alert('Desculpe insira um email para continuar')
+        txterr.style.marginLeft = '100px'
+        txterr.innerHTML = 'insira um email'
         
     }else{
         if(regexEmail.test(msg)){
             window.location.href = "confirm.html"
             window.location.href = `confirm.html?mensagem=${msg}`;
         }else{
-            window.alert('Desculpe mas email invalido.')
+            txterr.style.marginLeft = '100px'
+            txterr.innerHTML = 'Email invalido'
+
         }
         
     }
