902b1656499457b9b41e5cbbdd2d20c310028548
Gabriel xavier <92322369+Gabrieldaybion@users.noreply.github.com>
2023-10-26 20:47:25 -0300

    sobe vai

--- Meus projetos/DEGABIONTWESS/daybion-page/JS/transition.js
+++ Meus projetos/DEGABIONTWESS/daybion-page/JS/transition.js
@@ -1,9 +1,27 @@
 import {typeWrite} from "./script.js";
 
 
-function verifica(){
+function verifica_html(){
     var txt = document.getElementById('s-html-txt');
     var scaixa = document.getElementById('s-caixa');
+    var s_html = document.querySelector('.s-html');
+    scaixa.addEventListener('mouseenter',function(){
+        console.log('Mouse em cima do elemento');
+        s_html.style.height = '20%'
+        txt.style.display = 'inline'
+        
+        
+    })
+    scaixa.addEventListener('mouseleave',function(){
+        console.log('Mouse saiu');
+        s_html.style.height = '90%'
+        txt.style.display = 'none';
+    })
+}
+
+function verifica_css(){
+    var txt = document.getElementById('s-css-txt');
+    var scaixa = document.getElementById('s-caixa2');
     scaixa.addEventListener('mouseenter',function(){
         console.log('Mouse em cima do elemento');
         txt.style.display = 'inline'
@@ -12,9 +30,18 @@ function verifica(){
         console.log('Mouse saiu');
         txt.style.display = 'none';
     })
-    
-    
-
+}
+function verifica_js(){
+    var txt = document.getElementById('s-js-txt');
+    var scaixa = document.getElementById('s-caixa3');
+    scaixa.addEventListener('mouseenter',function(){
+        console.log('Mouse em cima do elemento');
+        txt.style.display = 'inline'
+    })
+    scaixa.addEventListener('mouseleave',function(){
+        console.log('Mouse saiu');
+        txt.style.display = 'none';
+    })
 }
 
 /* Para escrever o Header */
@@ -24,4 +51,6 @@ function escreva_header(esc){
 }
 const titulo = document.querySelector('h1#tt');
 escreva_header(titulo);
-verifica()
+verifica_html()
+verifica_css()
+verifica_js()
