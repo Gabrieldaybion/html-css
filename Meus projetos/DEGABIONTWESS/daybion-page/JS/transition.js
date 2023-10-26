0189586f979129308705f8009ee113458ab48858
Gabriel xavier <92322369+Gabrieldaybion@users.noreply.github.com>
2023-10-26 19:53:29 -0300

    pt fim

--- Meus projetos/DEGABIONTWESS/daybion-page/JS/transition.js
+++ Meus projetos/DEGABIONTWESS/daybion-page/JS/transition.js
@@ -1,43 +1,22 @@
 import {typeWrite} from "./script.js";
 
-function html_txt(){
 
-    console.log('txt aparece');
-    var txt = document.getElementById('s-html-txt')
+function verifica(){
+    var txt = document.getElementById('s-html-txt');
     var scaixa = document.getElementById('s-caixa');
-    scaixa.onpointerover = ()=>{
+    scaixa.addEventListener('mouseenter',function(){
+        console.log('Mouse em cima do elemento');
         txt.style.display = 'inline'
-        
-    };
+    })
+    scaixa.addEventListener('mouseleave',function(){
+        console.log('Mouse saiu');
+        txt.style.display = 'none';
+    })
     
-};
-function html_txt_desable(){
     
-  
-        console.log('txt aparece'); 
-        console.log('txt some');
 
-        var txt = document.getElementById('s-html-txt') 
-        var scaixa = document.getElementById('s-html-txt')
+}
 
-        function saiu(mouse){
-          if (mouse = "mouseout")
-          return 
-        }
-
-        if(saiu() ==  "mouseout"){
-            saiu()
-            console.log('A mensagem saiu')
-        }else{
-            console.log('Deu algum b.o');
-        }
-        scaixa.addEventListener("mouseout",()=>{
-            txt.style.display = 'none';
-        })
-        
-        
-      
-};
 /* Para escrever o Header */
 function escreva_header(esc){
     
@@ -45,5 +24,4 @@ function escreva_header(esc){
 }
 const titulo = document.querySelector('h1#tt');
 escreva_header(titulo);
-html_txt()
-html_txt_desable()
+verifica()
