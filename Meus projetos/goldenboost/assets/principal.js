c34574cb8a5cadc522a1dc329d2fef7d3106bf15
Gabriel xavier <92322369+Gabrieldaybion@users.noreply.github.com>
2024-06-02 00:15:00 -0300

    up feito

--- Meus projetos/goldenboost/assets/principal.js
+++ Meus projetos/goldenboost/assets/principal.js
@@ -1,1 +1,14 @@
 console.log('hello ')
+function typeWrite(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra,i) => {
+        setTimeout(()=> 
+            elemento.innerHTML += letra, 289 * i)
+    } );
+}
+
+
+const titulo = document.querySelector('h1#tt');
+
+typeWrite(titulo);
